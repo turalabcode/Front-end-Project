df8b353badfa9ab9103a6163be0af02f4047cca7
turalabcode <turalab@code.edu.az>
2020-10-01 19:03:19 +0400

    Cities, Choose, Silva

---
+++ JS/main.js
@@ -0,0 +1,36 @@
+$( document ).ready(function() {
+    $(".owl-one").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+          0: {
+            items: 1,
+          },
+          600: {
+            items: 2,
+          },
+          1000: {
+            items: 3,
+          },
+        },
+      });
+
+      $(".my-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        items: 1,
+        responsive: {
+          0: {
+            items: 1,
+          },
+          600: {
+            items: 1,
+          },
+          1000: {
+            items: 1,
+          },
+        },
+      });
+})
